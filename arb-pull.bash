64fd887c1888c5dc27d25ce5031e81545b08e7c4
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2024-12-31 18:59:40 +0900

    テスト用にコメントアウトしたいた部分を戻した．

--- arb-pull.bash
+++ arb-pull.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 ORIGINAL_IMAGE=airobotbook/ros2-desktop-ai-robot-book-humble:latest
 MY_IMAGE=ros2-desktop-ai-robot-book-humble:mine
-# docker pull $ORIGINAL_IMAGE
-# if [ $? -gt 0 ]; then
-#     echo "イメージの取得に失敗"
-#     exit 1
-# fi
+docker pull $ORIGINAL_IMAGE
+if [ $? -gt 0 ]; then
+    echo "イメージの取得に失敗"
+    exit 1
+fi
 ID=$(docker images --format "{{.ID}}" $ORIGINAL_IMAGE)
 docker tag $ID  $MY_IMAGE
 docker rmi $ORIGINAL_IMAGE
