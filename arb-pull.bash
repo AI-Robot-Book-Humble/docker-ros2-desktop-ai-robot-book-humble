140db9cb0023bc902406c8698d0d7287c739b477
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2025-01-02 20:13:40 +0900

    元になるDockerイメージがすでに存在する場合に対応するコードを追加

--- arb-pull.bash
+++ arb-pull.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 ORIGINAL_IMAGE=airobotbook/ros2-desktop-ai-robot-book-humble:latest
 MY_IMAGE=ros2-desktop-ai-robot-book-humble:mine
-docker pull $ORIGINAL_IMAGE
+ID=$(docker image ls -q $ORIGINAL_IMAGE)
+if [ -z "$ID" ]; then
+    docker pull $ORIGINAL_IMAGE
+else
+    echo "$ORIGINAL_IMAGEはすでにローカルに存在"
+fi
 if [ $? -gt 0 ]; then
     echo "イメージの取得に失敗"
     exit 1
 fi
-ID=$(docker images --format "{{.ID}}" $ORIGINAL_IMAGE)
+ID=$(docker image ls -q $ORIGINAL_IMAGE)
 docker tag $ID  $MY_IMAGE
 docker rmi $ORIGINAL_IMAGE
--- arb-pull.bat
+++ arb-pull.bat
@@ -2,11 +2,16 @@
 setlocal
 set ORIGINAL_IMAGE=airobotbook/ros2-desktop-ai-robot-book-humble:latest
 set MY_IMAGE=ros2-desktop-ai-robot-book-humble:mine
-docker pull %ORIGINAL_IMAGE%
+for /f "usebackq" %%A in (`docker image ls -q %ORIGINAL_IMAGE%`) do set ID=%%A
+if [%ID%] == [] (
+    docker pull %ORIGINAL_IMAGE%
+) else (
+    echo %ORIGINAL_IMAGE%はすでにローカルに存在
+)
 if errorlevel 1 (
     echo "イメージの取得に失敗"
     exit 1
 )
-for /f "usebackq" %%A in (`docker images --format "{{.ID}}" %ORIGINAL_IMAGE%`) do set ID=%%A
+for /f "usebackq" %%A in (`docker image ls -q %ORIGINAL_IMAGE%`) do set ID=%%A
 docker tag %ID% %MY_IMAGE%
 docker rmi %ORIGINAL_IMAGE%
